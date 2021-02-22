a73a41c877b365646b457e655cf7827e29ea7106
blairrorani <blair@rorani.com>
2021-02-22 20:03:58 +1100

    add tailwindcss

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+# define class named [Route]Controller and inherti from ApplicationController
+class AboutController < ApplicationController
+  # define an action named [action from route, i.e. index]
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # If there is a GET /about request, then route the request to the "about" controller and the "index" action
   get "about", to: "about#index"
+
+  root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
